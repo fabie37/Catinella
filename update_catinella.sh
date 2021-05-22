015c3a36abcdc78cc0790ad323b1fb5308896c84
Fabrizio <fabiecatinella@gmail.com>
2021-05-22 19:14:43 +0100

    [clean] tidy up boiler code

---
+++ update_catinella.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Variables
+
+domain="www.catinella.co.uk"
+port=80
+project_name="Catinella"
+public_dir="/var/www"
+git_url="https://github.com/fabie37/Catinella.git"
+
+echo
+echo $(date)
+echo "Updating the $project_name website. Hold on..."
+
+if [ -d "$public_dir/$project_name" ]
+then
+        echo "Deleting old project folder..."
+        rm -rf "$public_dir/$project_name"
+else
+        echo "Public dir:$public_dir is clean."
+fi
+
+echo "Downloading latest project."
+cd $public_dir
+git clone $git_url
+cd "$project_name"
+npm install
+npm run build
+echo "Alright! Go to $domain to test it out"
+serve -s build -p 80
