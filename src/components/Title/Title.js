208610fc3411054d94513a5673df50d0bb13da57
Fabrizio <fabiecatinella@gmail.com>
2021-05-22 20:38:14 +0100

    [feat] added underline to main title and transitions to main page

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './Title.css';
 
 const Title = ({ text }) => {
-    return <div className='Title'>{text}</div>;
+    return (
+        <div className='Title'>
+            <div className='Text'>{text}</div>
+            <hr noshade className='Line' />
+        </div>
+    );
 };
 
 export default Title;
