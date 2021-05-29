2cb281308394393a4330d6614b79b783ba014e3c
Fabrizio <fabiecatinella@gmail.com>
2021-05-29 19:55:31 +0100

    [rfc] replaced magic numbers in floaty.js

--- src/components/Floaty/Floaty.js
+++ src/components/Floaty/Floaty.js
@@ -1,27 +1,29 @@
-import React from "react";
-import "./Floaty.css";
+import React from 'react';
+import './Floaty.css';
 
 const Floaty = ({ floaty_list }) => {
-  return (
-    <div className="Floaty-Row">
-      {floaty_list.map((text, index) => {
-        return (
-          <div
-            key={index}
-            className="Floaty-Text"
-            style={{
-              animation: "fade_in 3s ease-in-out ".concat(
-                index + 2,
-                "s forwards"
-              ),
-            }}
-          >
-            {text}
-          </div>
-        );
-      })}
-    </div>
-  );
+    const animation_delay = 1.2;
+    const animation_duration = 1;
+
+    return (
+        <div className='Floaty-Row'>
+            {floaty_list.map((text, index) => {
+                return (
+                    <div
+                        key={index}
+                        className='Floaty-Text'
+                        style={{
+                            animation: `fade_in ${animation_duration}s ease-in-out ${
+                                index + animation_delay
+                            }s forwards`,
+                        }}
+                    >
+                        {text}
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Floaty;
