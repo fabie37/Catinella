2bc3671cd6d7ab570c6598662a0a2ff518f79282
Fabrizio <fabiecatinella@gmail.com>
2022-04-23 13:46:25 +0100

    [update] Added SEO meta-tags to site.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Body from "./components/Body/Body";
 import Page from "./components/Page/Page";
 import AboutBody from "./data/AboutBody";
 import {contacts} from "./data/ContactDetails";
+import {meta_title, meta_description, meta_keywords} from "./data/metadata";
+import {Helmet, HelmetProvider} from 'react-helmet-async';
 import React from "react";
 
 const App = () => {
@@ -20,20 +22,31 @@ const App = () => {
 
 
   return (
-    <div className="App">
-      <Page>
-        <Title key="Title" text={title} align='Center'></Title>
-        <Floaty key="Floaty-Bar" floaty_list={floaty_list}></Floaty>
-      </Page>
-      <Page>
-        <Title key="Title" text={about} align='Right'></Title>
-        <Body key="Body" align='Right' text={AboutBody}></Body>
-      </Page>
-      <Page>
-        <Title key="Title" text={contact} align='Left'></Title>
-        <Body key="Body" align='Left' text={contacts}></Body>
-      </Page>
-    </div>
+    <HelmetProvider>
+
+      {/* SEO Stuff */}
+      <Helmet>
+        <title>{meta_title}</title>
+        <meta name="description" content={meta_description}></meta>
+        <meta name="keywords" content={meta_keywords}></meta>
+      </Helmet>
+
+      {/* The Application */}
+      <div className="App">
+        <Page>
+          <Title key="Title" text={title} align='Center'></Title>
+          <Floaty key="Floaty-Bar" floaty_list={floaty_list}></Floaty>
+        </Page>
+        <Page>
+          <Title key="Title" text={about} align='Right'></Title>
+          <Body key="Body" align='Right' text={AboutBody}></Body>
+        </Page>
+        <Page>
+          <Title key="Title" text={contact} align='Left'></Title>
+          <Body key="Body" align='Left' text={contacts}></Body>
+        </Page>
+      </div>
+    </HelmetProvider>
   );
 };
 
---
+++ src/data/metadata.js
@@ -0,0 +1,5 @@
+const meta_title = "Fabrizio Catinella - Software Engineer"
+const meta_description = "Fabrizio Catinella - This is my personal website where you can get my contract details and get to know me more. I am interested in building anything and everything - as long as it provides a challenge and tests our understanding then I am game."
+const meta_keywords = "fabrizio, catinella, software, developer, full stack, blockchain, university of glasgow, glasgow, computing science, github, student, github, git, programming, programmer"
+
+export {meta_title, meta_description, meta_keywords}
