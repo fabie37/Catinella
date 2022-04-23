05b0c38e1ee7cb378d8cfd95fb37cad50bfa101b
Fabrizio <fabiecatinella@gmail.com>
2022-04-23 12:04:33 +0100

    [update] Updated about me to include blockchain and finance

--- src/data/AboutBody.js
+++ src/data/AboutBody.js
@@ -1,3 +1,3 @@
-const AboutBody = "Computing Science Student @ University of Glasgow. \n Work experience in full stack and embedded systems. \n Current interests involve: Building Drones, Machine Learning, Functional Programming, Start-ups."
+const AboutBody = "Computing Science Student @ University of Glasgow. \n Work experience in full stack and embedded systems. \n Current interests involve: Building Drones, Machine Learning, Blockchain, Finance and Start-ups."
 
 export default AboutBody
