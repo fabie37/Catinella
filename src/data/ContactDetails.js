8426a5dd4f76d7b55083d0107d9a0d6db2153f77
Fabrizio <fabiecatinella@gmail.com>
2022-04-23 12:41:21 +0100

    [update] Removed bad https from url in github

--- src/data/ContactDetails.js
+++ src/data/ContactDetails.js
@@ -1,7 +1,7 @@
 const phone = "Phone: 07450 856481"
 const email = "Email: fabrizio@catinella.co.uk"
 const linkedin = "Linkedin: linkedin.com/in/fabrizio-catinella/"
-const github = "Github: https://github.com/fabie37"
+const github = "Github: github.com/fabie37"
 
 const contacts = [phone,email,linkedin,github].reduce((p,n) => p + '\n' + n, '').replace(/^\n(?<=.*)/gm, '')
 
